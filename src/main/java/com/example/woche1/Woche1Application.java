d863f4cb813b99611ebf51829b87e079a1099bc6
Lukas Gerstacker <lukas-marius.gerstacker@informatik.hs-fulda.de>
2023-11-18 00:44:26 +0100

    update week 5

--- src/main/java/com/example/woche1/Woche1Application.java
+++ src/main/java/com/example/woche1/Woche1Application.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @SpringBootApplication
@@ -20,6 +18,7 @@ public class Woche1Application {
 }
 
 @RestController
+@CrossOrigin
 class FormController {
 
   @Autowired
