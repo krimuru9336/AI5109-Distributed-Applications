1a42add1fa3c20a2d761898fe1d33e163bdb1920
Jonas Wagner <jonas.wagner@informatik.hs-fulda.de>
2023-11-24 19:06:45 +0100

    Week 5: Update Controller, index.html & application.properties

--- src/main/java/com/hsfulda/distributedapplications1/Contacts/ContactController.java
+++ src/main/java/com/hsfulda/distributedapplications1/Contacts/ContactController.java
@@ -1,33 +1,29 @@
 package com.hsfulda.distributedapplications1.Contacts;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/contacts")
+@CrossOrigin(origins = "http://20.102.110.117")
 public class ContactController {
 
     @Autowired
     private ContactRepository contactRepository;
 
-    @GetMapping("/")
-    public String index(Model model) {
-        List<Contact> contacts = contactRepository.findAll();
-        model.addAttribute("contacts", contacts);
-        model.addAttribute("newContact", new Contact());
-        return "index";
+    @GetMapping
+    public List<Contact> getAllContacts() {
+        return contactRepository.findAll();
     }
 
-    @PostMapping("/contacts")
-    public String addContact(@ModelAttribute Contact contact) {
-        contactRepository.save(contact);
-        return "redirect:/";
+    @PostMapping
+    public Contact addContact(@RequestBody Contact contact) {
+        return contactRepository.save(contact);
     }
 
     /*
-    Jonas Wagner - 1315578 - 28.10.2023
+    Jonas Wagner - 1315578 - 24.11.2023
      */
 }
