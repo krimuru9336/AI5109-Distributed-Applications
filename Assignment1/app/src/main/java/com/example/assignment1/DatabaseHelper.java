f19bf6c52a0a0b40c3446509d2a4a6c6f394c305
Abbas Rabbani <mail.abbas.rabbani@gmail.com>
2023-12-13 00:25:41 +0100

    Project_week1

---
+++ Assignment1/app/src/main/java/com/example/assignment1/DatabaseHelper.java
@@ -0,0 +1,53 @@
+package com.example.assignment1;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "mydatabase.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_NAME = "mytable";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_TEXT_VALUE = "text_value";
+
+    private static final String DATABASE_CREATE =
+            "CREATE TABLE " + TABLE_NAME + "(" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_TEXT_VALUE + " TEXT NOT NULL);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public long insertData(String textValue) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TEXT_VALUE, textValue);
+        long rowId = db.insert(TABLE_NAME, null, values);
+        db.close();
+        return rowId;
+    }
+
+    public Cursor retrieveData() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {COLUMN_ID, COLUMN_TEXT_VALUE};
+        Cursor cursor = db.query(TABLE_NAME, columns, null, null, null, null, null);
+        return cursor;
+    }
+}
---
+++ Assignment1/app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.assignment1;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private DatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        dbHelper = new DatabaseHelper(this);
+
+        Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveToDatabase();
+            }
+        });
+
+        Button retrieveButton = findViewById(R.id.retrieveButton);
+        retrieveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                retrieveFromDatabase();
+            }
+        });
+    }
+
+    private void saveToDatabase() {
+        String inputText = editText.getText().toString();
+
+        if (!inputText.isEmpty()) {
+            long rowId = dbHelper.insertData(inputText);
+
+            if (rowId != -1) {
+                showToast("Data saved to DB with ID: " + rowId);
+            } else {
+                showToast("Failed to save data to DB");
+            }
+        } else {
+            showToast("Please enter text");
+        }
+    }
+
+    private void retrieveFromDatabase() {
+        Cursor cursor = dbHelper.retrieveData();
+
+        if (cursor.moveToFirst()) {
+            do {
+                String textValue = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_TEXT_VALUE));
+                showToast("Retrieved from DB: " + textValue);
+            } while (cursor.moveToNext());
+        } else {
+            showToast("No data in DB");
+        }
+
+        cursor.close();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
