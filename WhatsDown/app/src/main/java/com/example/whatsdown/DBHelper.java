985851b6e30b9e1dac187785129b8debda2a8210
Rishabh Goswami <rish.xi@gmail.com>
2023-12-19 16:50:35 +0400

    feat: added source code  for whatsdown app for module 1

---
+++ WhatsDown/app/src/main/java/com/example/whatsdown/DBHelper.java
@@ -0,0 +1,31 @@
+package com.example.whatsdown;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "mydatabase.db";
+    private static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "mytable";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_TEXT = "text";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableQuery = "CREATE TABLE " + TABLE_NAME + "(" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TEXT + " TEXT)";
+        db.execSQL(createTableQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
