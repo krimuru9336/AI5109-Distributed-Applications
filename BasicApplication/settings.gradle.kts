d2ce0652929e8775e553f6c1807bb8b4a2863957
oshadhi-sam <oshadhi-gangani.samarasinghe@informatik.hs-fulda.de>
2023-12-19 21:46:51 +0100

    basic android app setup

---
+++ BasicApplication/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Basic Application"
+include(":app")
+ 
