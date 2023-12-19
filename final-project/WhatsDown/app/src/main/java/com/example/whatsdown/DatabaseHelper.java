2d7ff7f9a5b1968768678ba7097473ba02e41093
Seshenya-Weerasinghe <seshenya.weerasinghe@informatik.hs-fulda.de>
2023-12-19 21:41:18 +0100

    Changed folder structure with final-project added

---
+++ final-project/WhatsDown/app/src/main/java/com/example/whatsdown/DatabaseHelper.java
@@ -0,0 +1,35 @@
+package com.example.whatsdown;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "mydatabase.db";
+    private static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "whatsdown";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_NAME + " TEXT, " +
+                    "timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
---
+++ final-project/WhatsDown/app/src/main/java/com/example/whatsdown/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.whatsdown;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText inputEditText;
+    private DatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputEditText = findViewById(R.id.inputEditText);
+        Button saveButton = findViewById(R.id.saveButton);
+        Button retrieveButton = findViewById(R.id.retrieveButton);
+
+        dbHelper = new DatabaseHelper(this);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveToDatabase(inputEditText.getText().toString());
+                inputEditText.getText().clear();
+            }
+        });
+
+        retrieveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retrieveFromDatabase();
+            }
+        });
+    }
+
+    private void saveToDatabase(String text) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COLUMN_NAME, text);
+        db.insert(DatabaseHelper.TABLE_NAME, null, values);
+        Toast.makeText(this, "Your name has been successfully saved!", Toast.LENGTH_SHORT).show();
+        db.close();
+    }
+
+    private void retrieveFromDatabase() {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.query(
+                DatabaseHelper.TABLE_NAME,
+                new String[]{DatabaseHelper.COLUMN_NAME},
+                null,
+                null,
+                null,
+                null,
+                "timestamp DESC",
+                "1"
+        );
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int columnIndex = cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME);
+
+            if (columnIndex != -1) {
+                String text = cursor.getString(columnIndex);
+                Toast.makeText(this, "Hello Dear " + text, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Couldn't find any names in the DB", Toast.LENGTH_SHORT).show();
+            }
+
+            cursor.close();
+        } else {
+            Toast.makeText(this, "Couldn't find any names in the DB", Toast.LENGTH_SHORT).show();
+        }
+
+        db.close();
+    }
+}
