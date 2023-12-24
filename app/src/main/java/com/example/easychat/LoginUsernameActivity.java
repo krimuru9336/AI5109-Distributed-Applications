d79ea33b15d49d6cc73e54d9e4e6d1b7f44ac730
fdai7673 <usama.tahir@informatik.hs-fulda.de>
2023-12-24 21:53:07 +0100

    creting  splach and login activities  and connecting to firebase

---
+++ app/src/main/java/com/example/easychat/LoginUsernameActivity.java
@@ -0,0 +1,14 @@
+package com.example.easychat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginUsernameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_username);
+    }
+}
