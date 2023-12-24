476d52db78aa5b825fbb79e7f6f92d4fe468d7f9
Suhaila7995 <152189280+Suhaila7995@users.noreply.github.com>
2023-12-24 18:41:58 +0530

    Create UserRepository.java

---
+++ WEEKLY-UPLOADS/Week1&2/src/main/java/com/example/app/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
+
+/**
+ * Week 1 - Basic Spring Boot Setup
+ * Created By: Suhaila Kondappilly Aliyar
+ * Created on: 3rd November 2023
+ * Matriculation Number:1492822
+ */
