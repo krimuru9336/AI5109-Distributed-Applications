fec8be98a8f722f0081134cddfdd2c4a96467a88
DipeshK1999 <148889685+DipeshK1999@users.noreply.github.com>
2024-01-01 23:13:25 +0100

    server deployed on azure and logout implemented

--- ChitChat/client/baseUrl.js
+++ ChitChat/client/baseUrl.js
@@ -1,2 +1,4 @@
-export const baseUrl = "http://192.168.0.191:8000/"
-export const socketUrl = "192.168.0.191:8000/"
+// export const baseUrl = "http://192.168.0.191:8000/"
+// export const socketUrl = "192.168.0.191:8000/"
+export const baseUrl = "http://4.231.172.21/"
+export const socketUrl = "4.231.172.21/"
--- ChitChat/client/screens/home/index.js
+++ ChitChat/client/screens/home/index.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 import UserList from './components/UserList';
 import { baseUrl } from "../../baseUrl";
 import AppToast from '../../components/AppToast';
 import { getUserFromLocalStorage } from '../../helper';
 import { useSocket } from '../../SocketContext';
+import AppButton from '../../components/AppButton';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const HomeScreen = ({ navigation }) => {
     const [users, setUsers] = useState([]);
@@ -25,13 +28,11 @@ const HomeScreen = ({ navigation }) => {
     };
 
     useEffect(() => {
-
         setSocketConnection();
         getUsers();
     }, []);
 
     const getUsers = () => {
-
         axios.get(`${baseUrl}user/`)
             .then((res) => {
                 if (res.data) {
@@ -53,8 +54,17 @@ const HomeScreen = ({ navigation }) => {
         setSocketConnection();
     }
 
+    const logout = async () => {
+        await AsyncStorage.removeItem('user');
+        navigation.reset({
+            index: 0,
+            routes: [{ name: 'Sign Up' }],
+        });
+    };
+
     return (
         <View style={styles.container}>
+            <AppButton onPress={logout} style={styles.logoutButton} text='Logout' />
             <Text style={styles.header}>Users</Text>
             <UserList user={userRef.current} users={users} handleClick={handleClick} />
             {error && <AppToast setError={setError} visible={true} text={error} />}
@@ -71,6 +81,16 @@ const styles = StyleSheet.create({
         fontSize: 24,
         marginBottom: 20,
     },
+    logoutButton: {
+        position: 'absolute',
+        top: 20,
+        right: 20,
+        padding: 10,
+        backgroundColor: '#FF4A47',
+        borderColor: 'red',
+        borderWidth: 1,
+        borderRadius: 5,
+    }
 });
 
 export default HomeScreen;
