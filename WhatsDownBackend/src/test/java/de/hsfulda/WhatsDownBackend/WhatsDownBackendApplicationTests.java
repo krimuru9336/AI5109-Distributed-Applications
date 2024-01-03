7a260492d554abfad7e86ae68099267676e17943
Jonas Wagner <jonas.wagner.2001@gmx.de>
2024-01-03 13:32:32 +0100

    Add tests

---
+++ WhatsDownBackend/src/test/java/de/hsfulda/WhatsDownBackend/WhatsDownBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsfulda.WhatsDownBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsDownBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
