0c951c851d732af4f40a0f43766ce03b919dd6cc
fdai7673 <usama.tahir@informatik.hs-fulda.de>
2024-01-18 19:08:31 +0100

    adding main activity

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,33 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.2.0" apply false
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:7.4.2'
+        classpath 'com.google.gms:google-services:4.3.13'
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        maven { url 'https://jitpack.io' }
+        mavenCentral()
+        //noinspection JcenterRepositoryObsolete
+        jcenter()
+    }
+    ext {
+        _compileSdkVersion = 31
+        _minSdkVersion = 21
+        _targetSdkVersion = 31
+    }
+}
+
+task clean(type: Delete) {
+    delete rootProject.buildDir
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,1 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "My Application"
-include(":app")
+include ':app', ':record-view'
