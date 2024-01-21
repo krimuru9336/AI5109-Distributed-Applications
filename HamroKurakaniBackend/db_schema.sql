7c7602420cad8fc81082d89d0bed5cced4ea4154
fdai8002 <samman.adhikari@informatik.hs-fulda.de>
2024-01-21 17:00:04 +0100

    sending messages to specific users implemented in backend

--- HamroKurakaniBackend/db_schema.sql
+++ HamroKurakaniBackend/db_schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
     id VARCHAR(255),
     username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
--- HamroKurakaniBackend/server.py
+++ HamroKurakaniBackend/server.py
@@ -1,24 +1,37 @@
+import datetime
 from flask import Flask, request,jsonify
-from flask_socketio import SocketIO,emit
+from flask_socketio import SocketIO,emit,send, join_room
 from flask_cors import CORS
 from dotenv import load_dotenv
 import mysql.connector
 from mysql.connector import errorcode
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 import os
 import uuid
+from flask_cors import CORS
 
 load_dotenv()
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'secret!'
-CORS(app,resources={r"/*":{"origins":"*"}})
-socketio = SocketIO(app,cors_allowed_origins="*")
 
 dbHost = os.getenv("DB_HOST")
 dbUser = os.getenv("DB_USER")
 dbPassword = os.getenv("DB_PASSWORD")
 dbName = os.getenv("DB_NAME")
+
+appSecret = os.getenv("APP_SECRET")
+
+app = Flask(__name__)
+CORS(app)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+jwt = JWTManager(app)
+
+app.config['SECRET_KEY'] = os.getenv("SOCKET_SECRET_KEY")
+CORS(app,resources={r"/*":{"origins":"*"}})
+socketio = SocketIO(app,cors_allowed_origins="*")
+
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 dbConfig = {
   'user': dbUser,
@@ -43,28 +56,35 @@ dbCur = dbCnx.cursor()
 
 # DB QUERIES
 insert_new_user = (
-"INSERT INTO users (id, username)"
-"VALUES (%s, %s)")
-select_user = "SELECT * from users WHERE id=%s"
+"INSERT INTO users (id, username, password)"
+"VALUES (%s, %s, %s)")
+select_user = "SELECT * from users WHERE username=%s"
 
-# @app.route("/http-call")
-# def http_call():
-#     """return JSON with string data as the value"""
-#     data = {'data':'This text was fetched using an HTTP call to server on render'}
-#     return jsonify(data)
+userIdToSid = {}
 
 @socketio.on("connect")
 def connected():
-    """event listener when client connects to the server"""
-    print(request.sid)
-    print("client has connected")
-    emit("connect",{"data":f"id: {request.sid} is connected"})
+    userId = request.args.get("uid")
+    print("client has connected with socket id:", request.sid)
+    userIdToSid[userId] = request.sid
 
 @socketio.on('data')
 def handle_message(data):
-    """event listener when client types a message"""
-    print("data from the front end: ",str(data))
-    emit("data",{'data':data, 'id':request.sid}, broadcast=True)
+    userSid = request.sid
+    recipientUid = data.get('recipientUid')
+    msg = data.get("message")
+    print(recipientUid)
+    if recipientUid:
+        recipientSid = userIdToSid[recipientUid]
+        roomName = f"user_room_{recipientSid}"
+        join_room(roomName)
+        print(f"Sending message: '{msg}' to room: '{roomName}'")
+        emit("data", {'message': msg, 'id': userSid}, room=recipientSid)
+    else:
+        print(f"Broadcasting message: {msg}")
+        # If recipientSid is not provided, broadcast the message to all connected clients
+        emit("data", {'message': msg, 'id': userSid}, broadcast=True)
+
 
 @socketio.on("disconnect")
 def disconnected():
@@ -72,25 +92,56 @@ def disconnected():
     print("user disconnected")
     emit("disconnect",f"user {request.sid} disconnected",broadcast=True)
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/", methods=["GET"])
+def getServerStatus():
+    return "Server is up!"      
+
+@app.route("/login", methods=['POST'])
 def login():
-    if request.method == 'POST':
-        data = request.json
-        username = data.get("username")
-        if(username == None):
-            return "Username is required!", 400
-        userUuid = uuid.uuid4()
-        userId = str(userUuid)
-        try:
-            dbCur.execute(insert_new_user, (userId, username))
-            dbCur.execute(select_user, (userId,))
-            users = dbCur.fetchall()
-            return users[0]
-        except Exception as ex:
-            if ex.errno == 1062:
-                return "Username already taken.", 400
-            else:
-                return f"Unexpected {ex=}, {type(ex)=}", 400
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    if(username == None):
+        return "Username is required!", 400
+    if(password == None):
+        return "Password is required!", 400
+    try:
+        dbCur.execute(select_user, (username,))
+        users = dbCur.fetchall()
+        user = users[0] if len(users) > 0 else None
+        print(generate_password_hash(password=password))
+        if user and check_password_hash(user[2], password):
+            access_token = create_access_token(identity=username, expires_delta=datetime.timedelta(days=30))
+            return jsonify({"message": "Login successfully", "accessToken": access_token}), 201
+        else:
+            return jsonify({"message": "Incorrect username or password"}), 400
+    except Exception as ex:
+        return f"Unexpected {ex=}, {type(ex)=}", 400
+            
+@app.route("/register", methods=['POST'])
+def register():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    
+    if(username == None):
+        return "Username is required!", 400
+    if(password == None):
+        return "Password is required!", 400
+    
+    userUuid = uuid.uuid4()
+    userId = str(userUuid)
+    hashedPassword = generate_password_hash(password=password)
+
+    try:
+        dbCur.execute(insert_new_user, (userId, username, hashedPassword))
+        dbCnx.commit()
+        return jsonify({"message": "User registered successfully"}), 201
+    except Exception as ex:
+        if ex.errno == 1062:
+            return "Username already taken.", 400
+        else:
+            return f"Unexpected {ex=}, {type(ex)=}", 400
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=False)
