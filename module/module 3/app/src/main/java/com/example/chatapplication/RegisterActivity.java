276d8c7a684c89d186157796c06e0213b1d3ec3f
SoyamHsFulda <abu-sadat.soyam@informatik.hs-fulda.de>
2024-01-28 18:41:58 +0100

    little modification in register activity java file

--- module/module 3/app/src/main/java/com/example/chatapplication/RegisterActivity.java
+++ module/module 3/app/src/main/java/com/example/chatapplication/RegisterActivity.java
@@ -1,21 +1,6 @@
 package com.example.chatapplication;
 
 
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_ACTIVE;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_CREATED_AT;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_EMAIL;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_ID;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_IMAGEURL;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_IS_ONLINE;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_PASSWORD;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_SEARCH;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_SIGNUP_TYPE;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_USERNAME;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_VERSION;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.IMG_DEFAULTS;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.REF_USERS;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.STATUS_ONLINE;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.TYPE_EMAIL;
 
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -24,7 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.bytesbee.firebase.chat.activities.managers.Utils;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
