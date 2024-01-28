4d5940be7196eadea584fac9683cbee094202bfd
SoyamHsFulda <abu-sadat.soyam@informatik.hs-fulda.de>
2024-01-28 18:41:28 +0100

    little modification on login activity java file

--- module/module 3/app/src/main/java/com/example/chatapplication/LoginActivity.java
+++ module/module 3/app/src/main/java/com/example/chatapplication/LoginActivity.java
@@ -1,21 +1,6 @@
 package com.example.chatapplication;
 
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_ACTIVE;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_CREATED_AT;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_EMAIL;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_ID;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_IMAGEURL;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_IS_ONLINE;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_PASSWORD;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_SEARCH;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_SIGNUP_TYPE;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_SOCIAL_TOKEN;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_USERNAME;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.EXTRA_VERSION;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.IMG_DEFAULTS;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.REF_USERS;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.STATUS_ONLINE;
-import static com.bytesbee.firebase.chat.activities.constants.IConstants.TYPE_GOOGLE;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -27,8 +12,7 @@ import android.widget.TextView;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import com.bytesbee.firebase.chat.activities.managers.Utils;
-import com.bytesbee.firebase.chat.activities.views.SingleClickListener;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
