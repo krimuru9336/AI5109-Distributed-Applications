aaf534b26352340b93c93fab15eeea7a7d1d6bf9
rahiluni23 <rahil.dutta@informatik.hs-fulda.de>
2024-03-26 12:48:33 +0530

    remove current user from group users list. only shows other memeber in the list

--- RahilChatApplication/app/src/main/java/com/example/rahilchatapplication/GroupUsers.kt
+++ RahilChatApplication/app/src/main/java/com/example/rahilchatapplication/GroupUsers.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.*
+import com.google.firebase.auth.FirebaseAuth
 
 class GroupUsers : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
@@ -13,11 +14,12 @@ class GroupUsers : AppCompatActivity() {
     private lateinit var groupName: String
     private val userList: ArrayList<User> = ArrayList()
     private lateinit var mDbRef: DatabaseReference
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_users)
-
+        mAuth = FirebaseAuth.getInstance()
         // Get the group name passed from the previous activity
         groupName = intent.getStringExtra("groupName") ?: ""
 
@@ -37,15 +39,18 @@ class GroupUsers : AppCompatActivity() {
             @SuppressLint("NotifyDataSetChanged")
             override fun onDataChange(snapshot: DataSnapshot) {
                 userList.clear()
+                val currentUserID = mAuth.currentUser?.uid // Get the ID of the current user
                 for (userSnapshot in snapshot.children) {
                     val userId = userSnapshot.key
-                    val userName = userSnapshot.child("name").getValue(String::class.java)
-                    val userEmail = userSnapshot.child("email").getValue(String::class.java)
+                    if (userId != currentUserID) { // Exclude the current user from the list
+                        val userName = userSnapshot.child("name").getValue(String::class.java)
+                        val userEmail = userSnapshot.child("email").getValue(String::class.java)
 
-                    userId?.let { id ->
-                        userName?.let { name ->
-                            userEmail?.let { email ->
-                                userList.add(User(name, email, id))
+                        userId?.let { id ->
+                            userName?.let { name ->
+                                userEmail?.let { email ->
+                                    userList.add(User(name, email, id))
+                                }
                             }
                         }
                     }
