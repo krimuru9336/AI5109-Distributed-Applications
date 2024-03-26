8a63409ce2de0c0a37deb26ec9361e6656f95c40
fdai7673 <usama.tahir@informatik.hs-fulda.de>
2024-03-26 16:57:22 +0100

    Users Activity Updated

---
+++ app/src/main/java/com/bytesbee/firebase/chat/activities/UsersActivity.java
@@ -0,0 +1,84 @@
+package com.bytesbee.firebase.chat.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.bytesbee.firebase.chat.activities.fragments.UsersFragment;
+
+import org.jetbrains.annotations.NotNull;
+
+public class UsersActivity extends BaseActivity {
+
+    private Fragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        final Toolbar mToolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(R.string.strUsers);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        if (savedInstanceState != null) {
+
+            fragment = getSupportFragmentManager().getFragment(savedInstanceState, "currentFragment");
+
+        } else {
+
+            fragment = new UsersFragment();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container_body, fragment).commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NotNull Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+
+        getSupportFragmentManager().putFragment(outState, "currentFragment", fragment);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        fragment.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
